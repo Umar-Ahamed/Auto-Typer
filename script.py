99f4f1dadfe3e8f20ea0201cae9301cb08afd199
XxUwUxX21 <66582928+XxUwUxX21@users.noreply.github.com>
2023-02-11 21:49:28 -0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,17 @@
+# You must have these two modules in order for the script to run
+import pyautogui
+import time
+
+# You can turn of the speed var by changing it into a comment
+# If you want the speed var on then dont change it to a comment
+speed = 0.25
+# You can change the (#) to what ever number you like.
+# This determines when the script will start typing after excuting the script.
+time.sleep(3)
+ # Create whatever txt file you want and make sure its in the same directory as the script.py
+for line in open("", encoding= "utf-8"):
+ 
+    pyautogui.typewrite(line, speed)
+     # Once you execute the script you will have to then press "enter" for the script to completely run.
+     # You can change the "enter" to whatever key you want. eg: "b"
+    pyautogui.press("enter")
